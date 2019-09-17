780d00bd37d5fffb69299a2c5773683e17e3f3c6
TR-1000 <t.l.ross@outlook.com>
2019-09-17 10:46:38 -0500

    Update db for heroku

--- gameified/settings.py
+++ gameified/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+ Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Heroku: Update database configuration from DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
